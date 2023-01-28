65c9cc800e5e453a85d4b03e8788eff5ac3984e4
Manan Patel <70314133+0xManan@users.noreply.github.com>
2023-01-29 00:54:51 +0530

    Update ChatBot.py

--- ChatBot.py
+++ ChatBot.py
@@ -1,28 +1,5 @@
 import openai
 
-class CodeExecutor(object):
-
-    def __init__(self, code_string="", code_object=None):
-        self.code_string = code_string
-        self.code_object = code_object
-        self.function_list = {}
-
-    def compile_code(self, code_string):
-        self.code_string = code_string
-        self.code_object = compile(self.code_string, "tmpfile", "exec")
-
-    def execute(self):
-        if self.code_object is not None:
-            resp = exec(self.code_object)
-            for co_name in self.code_object.co_names:
-                # Note: if the function name already exists in the function_list we would ignore
-                # it.
-                if co_name not in self.function_list:
-                    self.function_list[co_name] = eval(co_name)
-            return resp
-        else:
-            return None
-
 class ChatGPT:
     def __init__(self, api_key):
         self.api_key = api_key
@@ -30,8 +7,7 @@ class ChatGPT:
 
     def chat(self, prompt):
         completions = openai.Completion.create(
-            engine="text-davinci-003",
-            #engine="code-davinci-002",
+            engine="text-davinci-002",
             prompt=prompt,
             max_tokens=1024,
             n=1,
@@ -41,28 +17,9 @@ class ChatGPT:
         message = completions.choices[0].text
         return message
 
-def runner(code_executor, resp):
-    code_executor.compile_code(resp)
-    if code_executor.code_object is not None:
-        print(f"co_names -> {code_executor.code_object.co_names}")
-    code_executor.execute()
+# Instantiate the ChatGPT class with your API key
+chat = ChatGPT(API_KEY)
 
-def chat_and_run(chat_engine, code_executor, chat_message, should_run=True):
-    while True:
-        try:
-            resp = chat_engine.chat(chat_message)
-            print("#" * 30)
-            print(resp)
-            if should_run:
-                runner(code_executor, resp)
-            break
-        except Exception as e:
-            print(f"Run failed, try again. -> {e}")
-            continue
-
-def main():
-    chat = ChatGPT(API_KEY)
-    code_executor = CodeExecutor()
-chat_and_run(chat, code_executor,
-                 r"Develop a python 3 function that injects shell code into the process \"explorer.exe\" on a machine runs Windows OS. Include the relvant imports. No prelog.",
-                 should_run=False)
+# Use the chat() function to generate a response to a prompt
+response = chat.chat("Hello, how are you?")
+print(response)
